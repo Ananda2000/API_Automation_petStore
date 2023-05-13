4f7c7b3c1593e541891aafa7ba8e86c1131632fb
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2023-05-13 22:49:17 +0530

    This is api frame work 1st commit

---
+++ src/test/java/api/ResponseCodes/Statuscodes.java
@@ -0,0 +1,14 @@
+package api.ResponseCodes;
+
+public class Statuscodes {
+	public static int Response_status_codes_200=200; /// used in get call when the response is successfull
+	public static int Response_status_codes_201=201; //
+	public static int Response_status_codes_400=400;
+	public static int Response_status_codes_401=401;
+	public static int Response_status_codes_403=403;
+	public static int Response_status_codes_404=404;
+	public static int Response_status_codes_500=500;
+	
+	
+
+}
--- src/test/java/api/Tests/Store_module_testCases.java
+++ src/test/java/api/Tests/Store_module_testCases.java
@@ -1,23 +1,40 @@
 package api.Tests;
 
+import java.io.IOException;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import api.Endpoints.StoreEndpoint_Methods;
+import api.ResponseCodes.Statuscodes;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Store_module_testCases 
 {
+	ObjectMapper objmap = new ObjectMapper();
 	@Test(priority=1)
-	public void get()
+	public void get() throws IOException
 	{
 		
-		Response respss =StoreEndpoint_Methods.Get_method(3);
+		Response respss =StoreEndpoint_Methods.Get_method(7);
 		System.out.println(respss.getStatusCode());
-		System.out.println(respss.getBody().asString());
-		System.out.println(respss.getHeaders());
+		
+		Assert.assertEquals(respss.getStatusCode(), Statuscodes.Response_status_codes_200, "The Status code is not matching...");
 	
+			String str = respss.getBody().asString();
+			
+			JsonNode rootnode = objmap.readTree(str);
+			JsonNode jsonid = rootnode.path("id");
+			System.out.println(jsonid.asInt());
+			Assert.assertEquals(7, jsonid.asInt());
+			
+			
+			System.out.println(str);
 	/*	RestAssured.baseURI="https://petstore.swagger.io/v2/store/order/3";
 		
 		RequestSpecification request = RestAssured.given();
